c3921d42257c3dae17cd01010d999f0241348d49
Ahmed Elfiky <ahmed.elfiky@gameball.co>
2024-08-24 03:09:53 +0300

    Session II

--- SocialMediaApp/Models/Comment.cs
+++ SocialMediaApp/Models/Comment.cs
@@ -3,11 +3,11 @@ using static System.Net.Mime.MediaTypeNames;
 namespace SocialMediaApp.Models
 {
 
-    public class Comment
-    {
-        public int Id { get; set; }
-        public int PostId { get; set; }
-        public string Content { get; set; }
-
-    }
+	public class Comment
+	{
+		public int Id { get; set; }
+		public string Content { get; set; }
+		public int PostId { get; set; }
+		public Post Post { get; set; }
+	}
 }
--- SocialMediaApp/Program.cs
+++ SocialMediaApp/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SocialMediaApp;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add your DbContext configuration here
+builder.Services.AddDbContext<SocialMediaContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
